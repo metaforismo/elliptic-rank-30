b791eea122d3471201e9e14f3cad69bccc05ae92
metaforismo <39646696+metaforismo@users.noreply.github.com>
2026-08-07 17:22:06 +0200

    Add the Lean mathlib project

---
+++ lakefile.lean
@@ -0,0 +1,17 @@
+import Lake
+
+open Lake DSL
+
+package «elliptic-rank-30» where
+  leanOptions := #[
+    ⟨`autoImplicit, false⟩,
+    ⟨`warningAsError, true⟩
+  ]
+  moreServerOptions := #[⟨`linter.unusedVariables, false⟩]
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.32.2"
+
+@[default_target]
+lean_lib EllipticRank30 where
+  roots := #[`EllipticRank30]
